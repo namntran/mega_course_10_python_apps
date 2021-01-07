bc3974247b0ff7e6de7b97b25e29b66983b70d20
namntran <namtran78@gmail.com>
2021-01-07 11:25:34 +1000

    add variables

--- basics.py
+++ basics.py
@@ -1,3 +1,11 @@
 #first python program
+# add variable, variables are names to store values
 import datetime
-print("The date and time is", datetime.datetime.now())
+mynow = datetime.datetime.now()
+print(mynow)
+
+# variables can store objects
+mynumber = 10
+mystring = "Hello"
+
+print(mynumber, mystring)
